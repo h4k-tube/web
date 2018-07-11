fc848b827ef95b9c55d12b141c16f9550fd50668
Carlo Denaro <blackout_lair@yahoo.com>
2018-07-11 18:01:15 +0200

    Update MessageWidget.vue

--- src/MessageWidget.vue
+++ src/MessageWidget.vue
@@ -72,7 +72,7 @@ export default {
 
         let textForMatching = `${this.videoId}/${this.channel}/@${this.speakerTwitter}/${this.tags.map(tag => '#' + tag + '#')}`
 
-        axios.get(`https://raw.githubusercontent.com/watch-devtube/ads/master/ads.json?r=${Math.random()}`
+        axios.get(`https://raw.githubusercontent.com/h4k-tube/ads/master/ads.json?r=${Math.random()}`
         ).then(response => {
           this.ad = response.data.reverse().find(it => new RegExp(it.pattern, 'i').test(textForMatching))
         }).catch(error => {
